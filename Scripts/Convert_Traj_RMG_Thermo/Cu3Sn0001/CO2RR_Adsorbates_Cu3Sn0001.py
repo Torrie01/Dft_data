743e8a81bff7d91c099f775b4536122145c74e69
Torrie01 <t.asifor@yahoo.com>
2026-01-31 01:36:14 -0500

    Thermo-data for Cu3Sn Add DFT_data and thermochemistry adsorbate

---
+++ Scripts/Convert_Traj_RMG_Thermo/Cu3Sn0001/CO2RR_Adsorbates_Cu3Sn0001.py
@@ -0,0 +1,252 @@
+
+entry(
+    index = 0,
+    label = "CHOX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[1.74349313, 0.0148300711, -1.65536121e-05, 1.03768953e-08, -2.77422703e-12, -14068.2838, -8.10961736], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[7.60674062, -0.00396066968, 7.15100332e-06, -3.87857554e-09, 7.04121665e-13, -15569.5014, -37.7908267], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""CHOX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 1,
+    label = "COCHOX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[1.5729872, 0.0217424348, -1.9716008e-05, 9.06956375e-09, -1.66595904e-12, -30719.8207, -1.32467428], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[10.97892, -0.00618637215, 1.12347294e-05, -6.14858309e-09, 1.12432452e-12, -33231.7825, -49.4699731], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""COCHOX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 2,
+    label = "COOHX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[1.57291532, 0.0239770732, -2.79387799e-05, 1.65750947e-08, -3.92695945e-12, -48808.2078, -6.7526346], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[10.2432721, -0.0045092048, 8.09079692e-06, -4.35111478e-09, 7.85594402e-13, -50956.2429, -50.3765829], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""COOHX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 3,
+    label = "COX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[2.15374115, 0.00711964156, -1.13374895e-05, 1.00074377e-08, -3.47838511e-12, -19372.6808, -4.67420257], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[4.45424836, -0.00162230699, 2.9503086e-06, -1.61116576e-09, 2.93861513e-13, -19914.6116, -16.0376878], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""COX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 4,
+    label = "OCCOX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[2.41884657, 0.0278294959, -3.94623871e-05, 2.87941258e-08, -8.50641432e-12, -372426.829, -12.375979], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[11.0515201, -0.00331790688, 6.11806441e-06, -3.41065788e-09, 6.32647174e-13, -374455.902, -55.2010354], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCCOX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 5,
+    label = "OCH2CH3X",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[-0.152284561, 0.0246758342, -2.43851356e-06, -1.29041581e-08, 6.67636085e-12, -34192.1423, 5.96629218], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[15.8179079, -0.0159216272, 2.84306512e-05, -1.51970587e-08, 2.72736568e-12, -38790.3015, -77.4374611], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCH2CH3X""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 6,
+    label = "OCHCH2OHX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[1.99356406, 0.0241995463, -4.78429797e-06, -1.05616427e-08, 5.92903504e-12, -46029.7164, -1.71487788], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[16.7417542, -0.0135009155, 2.41336552e-05, -1.29213277e-08, 2.32303859e-12, -50255.8728, -78.6622202], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCHCH2OHX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 7,
+    label = "OCHCH2X",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[-0.183400356, 0.0310079073, -2.95156972e-05, 1.45656291e-08, -2.74782627e-12, -22740.0632, 0.906274793], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[13.0982869, -0.00985530564, 1.75924576e-05, -9.39357213e-09, 1.68474578e-12, -26189.5764, -66.6529776], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCHCH2X""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 8,
+    label = "OCHCH3X",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[0.142606623, 0.0146411987, 7.34600446e-06, -1.76014715e-08, 7.46841651e-12, -26798.1402, 5.82555102], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[11.9648147, -0.0130600783, 2.33307148e-05, -1.24794114e-08, 2.24071492e-12, -30332.9213, -56.5308757], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCHCH3X""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 9,
+    label = "OCHCHOHX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[1.31738489, 0.0337353549, -3.07765911e-05, 1.35163088e-08, -1.94956551e-12, -41257.9069, -0.331264019], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[15.8143526, -0.0101891781, 1.81561425e-05, -9.67051827e-09, 1.73209325e-12, -45043.1443, -74.2060751], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCHCHOHX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 10,
+    label = "OCHOCHX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[0.0579249054, 0.0344787757, -3.46894579e-05, 1.74808247e-08, -3.37110895e-12, -40845.0891, 4.97109633], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[13.9289506, -0.00853520745, 1.53752974e-05, -8.32061068e-09, 1.50852866e-12, -44422.9436, -65.4843799], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCHOCHX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
+
+entry(
+    index = 11,
+    label = "OCHOX",
+    molecule =
+"""
+""",
+    thermo = NASA(
+    polynomials = [
+        NASAPolynomial(coeffs=[1.67220889, 0.0144593325, -7.37932862e-06, -1.33628765e-09, 1.74722736e-12, -57413.8836, -1.50759757], Tmin=(300.0, 'K'), Tmax=(1000.0, 'K')),
+        NASAPolynomial(coeffs=[9.14577907, -0.00549354708, 9.94099598e-06, -5.41764493e-09, 9.87569455e-13, -59512.1847, -40.2901072], Tmin=(1000.0, 'K'), Tmax=(2000.0, 'K')),
+    ],
+    Tmin=(300.0, 'K'),
+    Tmax=(2000.0, 'K'),
+),
+    shortDesc = u"""OCHOX""",
+    longDesc =
+u"""Calculated by Torrie Asifor at Northeastern University using Statistical Mechanics. Based on DFT calculations by Colin Gallagher at Northeastern University
+""",
+)
+
